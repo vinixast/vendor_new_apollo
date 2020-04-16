af5d2cb907584bdac5ab30589cb3f804260c8f03
vinixast <63014441+vinixast@users.noreply.github.com>
2020-04-16 19:13:31 -0300

    Update apollo_lite-vendor-blobs.mk

--- apollo_lite-vendor-blobs.mk
+++ apollo_lite-vendor-blobs.mk
@@ -1,13 +1,13 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_x/vendor,system/vendor)
